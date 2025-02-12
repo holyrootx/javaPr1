96d400d71563985eb6fb937cfbc3c1195a318a21
holyrootx <to_be_it_dinosaur@naver.com>
2025-02-12 18:39:34 +0900

    first commit

---
+++ src/mms/member/dao/MemberDAO.java
@@ -0,0 +1,41 @@
+package mms.member.dao;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import mms.member.vo.Member;
+
+public class MemberDAO {
+	
+	static List<Member> memberList = new CopyOnWriteArrayList<Member>();
+
+	
+	public boolean insertNewMember(Member newMember) {
+		// 기능을 여기서 정의하고 
+		// 서비스에서 호출 
+		return memberList.add(newMember);
+	}
+	
+	public boolean showMember(List<Member> memberList, int memberId ) {
+		for(Member member : memberList) {
+			if (member.getId() == memberId) {
+				System.out.println(member);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean showAllMember(List<Member> memberList) {
+		for(Member member : memberList) {
+				System.out.println(member);
+			}
+		
+		return true;	
+	}
+	
+	public static List<Member> getMemberList() {
+		return memberList;
+	}
+	
+}
