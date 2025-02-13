c8eabbf7b501da6eb766b587f87fec586d8fb7ff
holyrootx <to_be_it_dinosaur@naver.com>
2025-02-13 12:51:01 +0900

    입력 잘못 됐을 때 대응 추가

--- src/mms/member/action/MemberShowAction.java
+++ src/mms/member/action/MemberShowAction.java
@@ -12,7 +12,18 @@ public class MemberShowAction implements Action{
 	public void execute(Scanner sc) throws Exception {
 		ConsoleUtil cu = new ConsoleUtil();
 		
-		int MemberId = cu.getMemberId(sc,"조회하실 회원 번호를 입력해주세요(전체는 0)");
+		int MemberId = 0;
+		do {
+			try {
+				MemberId = cu.getMemberId(sc,"조회하실 회원 번호를 입력해주세요(전체는 0)");
+				break;
+			}catch(Exception e) {
+				System.out.println("제발 숫자를 입력해주세요");
+				sc.nextLine(); // 버퍼를 비워줘야함 
+				continue;
+			}
+		} while(true);
+
 		// id 까지 받아왔고 0이면 전체 목록을 조회
 		// 아니면 특정 목록만 불러올것
 		boolean isShowSuccess = false;
@@ -20,7 +31,7 @@ public class MemberShowAction implements Action{
 		MemberShowService memberShowService = new MemberShowService();
 		if(MemberId == 0) {
 			// 전체 목록 조회
-			isShowSuccess = memberShowService.showAllMember(MemberId);
+			isShowSuccess = memberShowService.showAllMember();
 		} else {
 			isShowSuccess = memberShowService.showMember(MemberId);
 		}
--- src/mms/member/svc/MemberShowService.java
+++ src/mms/member/svc/MemberShowService.java
@@ -18,7 +18,7 @@ public class MemberShowService {
 		return isShowSuccess;
 		
 	}
-	public boolean showAllMember(int memberId) throws Exception {
+	public boolean showAllMember() throws Exception {
 		
 		// 서비스의 성공 유무를 나타냄
 		boolean isShowSuccess = false;
